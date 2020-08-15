ffbdf3ebf289aa51b45626e66d3924ca13c4e095
VA-007 <68072870+VA-007@users.noreply.github.com>
2020-08-15 20:45:50 +0530

    Delete 02_Node.JS_Modules_with_Examples.js

--- Notes/Backend Notes/02_Node.JS_Modules_with_Examples.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-let text = fs.readFileSync("Test.txt", "utf-8");
-text = text.replace("first", "lassan");
-
-console.log("The content of the file is")
-console.log(text);
-
-console.log("Creating a new file...")
-fs.writeFileSync("rohan.txt", text);
